9ee05604806841f645736ffa26146ebd85c019bc
永仁同志 <3082046177@qq.com>
2022-05-23 18:08:20 +0800

    5-23

--- infrastructure/security/src/main/java/com/yongren/timbo/infrastructure/security/SecurityApp.java
+++ infrastructure/security/src/main/java/com/yongren/timbo/infrastructure/security/SecurityApp.java
@@ -3,9 +3,11 @@ package com.yongren.timbo.infrastructure.security;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.yongren.timbo.infrastructure.security.mapper")
+@EnableDiscoveryClient
 public class SecurityApp {
     public static void main(String[] args) {
         SpringApplication.run(SecurityApp.class, args);
