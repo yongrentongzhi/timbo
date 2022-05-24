f3ce6b3589e30149979f200357cd9dd95e4b4ec4
永仁同志 <3082046177@qq.com>
2022-05-24 18:51:37 +0800

    5-24

--- application/service01/src/main/java/com/demo/controller/DemoController.java
+++ application/service01/src/main/java/com/demo/controller/DemoController.java
@@ -1,13 +1,19 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoController {
+    @Value("${userValue: default}")
+    private String userValue;
+
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello service01";
+        return "hello service01: "+userValue;
     }
 }
